230068d7a6f5a1faed6b3af9cbc1cb7e55eec74c
Hemul997 <nikitamoshkin1997@gmail.com>
2017-05-22 12:40:22 +0300

    Исправлены названия запросов

--- lab5/lab5.sql
+++ lab5/lab5.sql
@@ -1,8 +1,7 @@
 --1.1 Выбрать первые два значения из таблицы periodical, поля name_of_periodical
 SELECT TOP 2 name_of_periodical FROM periodical;
 
-
---2.1 Выбрать из таблицы film значения полей id_periodical, у которых поле name_of_periodical равно NULL
+--2.1 Выбрать из таблицы periodical значения полей id_periodical, у которых поле name_of_periodical равно NULL
 SELECT id_periodical FROM periodical WHERE name_of_periodical IS NULL;
 
 --2.2 Выбрать из таблицы subscriber значения поля fio_subscriber, не равные NULL
@@ -11,7 +10,7 @@ SELECT id_subscriber FROM subscriber WHERE fio_subscriber IS NOT NULL;
 --2.3 Выбрать из таблицы publisher поле id_publisher, значение которого от 1 до 3
 SELECT id_publisher FROM publisher WHERE id_publisher BETWEEN 1 AND 3;
 
---2.4 Выбрать из таблицы film_distribution значения, у которых id равен 1, 2 или 3
+--2.4 Выбрать из таблицы periodical значения, у которых id равен 1, 2 или 3
 SELECT * FROM periodical WHERE id_periodical In (1,2,3);
 
 --2.5 Выбрать поле fio_courier из таблицы courier, где id_courier равен 2
@@ -37,8 +36,8 @@ SELECT id_courier, fio_courier FROM courier ORDER BY 1 DESC;
 --4.1 Выбрать из таблицы periodical минимальную стоимость издания
 SELECT MIN(price) FROM periodical;
 
---4.2 Выбрать из таблицы courier номер телефона, наибольший по длине
-SELECT MAX(phone) FROM courier;
+--4.2 Выбрать из таблицы periodical максимальную стоимость издания
+SELECT MAX(price) FROM periodical;
 
 --4.3 Выбрать среднюю цену изданий
 SELECT AVG(price) FROM periodical;
@@ -54,12 +53,11 @@ SELECT DISTINCT (name_of_publisher) FROM publisher;
 SELECT Count(*) FROM (SELECT DISTINCT (name_of_publisher) FROM publisher)
 
 
---6.1 Выбрать из таблицы периодических изданий имя и общий заработок актёра и сгруппировать всё по имени
+--6.1 Выбрать из таблицы периодических изданий название издания и его суммарную стоимость. 
 SELECT name_of_periodical, Sum(price) FROM periodical GROUP BY name_of_periodical;
 
---6.2 Выбрать из таблицы периодических изданий название издания и его общую стоимость, при условии что стоимость журнала больше 0 рублей, сгруппировать по названию издания
+--6.2 Выбрать из таблицы периодических изданий название издания и его суммарную стоимость, при условии что стоимость одного журнала больше 0 рублей
 SELECT name_of_periodical, Sum(price) FROM periodical WHERE (price > 0) GROUP BY name_of_periodical;
 
-
---6.3 Выбрать из таблицы периодических изданий поля имя и общую стоимость изданий, при условии что общая стоимость журналов больше 50 рублей
+--6.3 Выбрать из таблицы периодических изданий поля имя издания и его суммарную стоимость, при условии что суммарная стоимость больше 100 рублей
 SELECT name_of_periodical, Sum(price) FROM periodical GROUP BY name_of_periodical HAVING (Sum(price) > 50);
